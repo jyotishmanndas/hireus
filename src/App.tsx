8411b49f6d64f1b14c7dbcf083c0f29abcc92f9c
Jyotishman Das <jyotishmand78@gmail.com>
2025-09-14 11:55:14 +0530

    Add a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import './App.css'
-import { Button } from './components/ui/button'
-
 function App() {
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <Button>jyoti</Button>
+    <div className="flex flex-col items-center min-h-screen bg-[url('/hreoPage.webp')] bg-cover bg-center">
+      <nav className="flex items-center justify-between w-full px-14 py-8">
+        <div className="flex items-center gap-4">
+          <div className="w-11 h-11">
+            <img src="/dark-logo.webp" alt="logo" />
+          </div>
+          <div className=" flex flex-col">
+            <div className="text-2xl">
+              Sheryians
+            </div>
+            <div className="text-2xl leading-5">
+              Coding School
+            </div>
+          </div>
+        </div>
+        <div className="text-3xl font-semibold">
+          2025
+        </div>
+      </nav>
     </div>
   )
 }
